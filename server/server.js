8bcfad557e842cd1e55f188ca1aae9c641becdd1
jarabicaj <jarabica.j@gmail.com>
2018-07-29 01:07:45 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -49,18 +49,20 @@ app.get('/todos/:id', (req, res) => {
    })
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send(`Id ${id} is invalid!`);
+    }
 
-
-app.post('/users', (req, res) => {
-    var user = new User({
-        email: req.body.email
-    });
-
-    user.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.send(e)
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }).catch((err) => {
+        res.status(400).send();
     })
 });
 
