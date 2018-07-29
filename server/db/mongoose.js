30d3ecbc72d34ab058d9fdecb6cee4ae36c1aebf
jarabicaj <jarabica.j@gmail.com>
2018-07-29 14:21:04 +0200

    deploy to heroku - editing url adress

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://dzejo333:cathering3@ds257851.mlab.com:57851/node-todo-api' || 'mongodb://localhost:27017/TodoApp');
 // mongoose.connect('mongodb://dzejo333:cathering3@ds257851.mlab.com:57851/node-todo-api');
 
 module.exports = {mongoose};
