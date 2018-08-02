91605313fe8b14d1d3c396a5689b9d9a7c551d65
jarabicaj <jarabica.j@gmail.com>
2018-08-02 21:12:34 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -96,6 +96,8 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+
+
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email','password']);
 
@@ -111,6 +113,23 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email,body.password).then((user) => {
+        //res.send(user);
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+
+
+
+});
+
 app.get('/users', (req, res) => {
     User.find().then((allUsers) => {
         res.send({allUsers})
@@ -118,8 +137,6 @@ app.get('/users', (req, res) => {
         res.status(400).send(e);
     })
 });
-
-
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
