1cf9b79fddffdb1e0e68c22e4467ce278855bffa
jarabicaj <jarabica.j@gmail.com>
2018-08-03 15:17:02 +0200

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -38,14 +38,17 @@ app.get('/todos', authenticate, (req, res) => {
     })
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
    var id = req.params.id;
 
    if(!ObjectID.isValid(id)) {
       return res.status(404).send((`ID: ${id} is not valid.`));
    }
 
-   Todo.findById(id).then((todo) => {
+   Todo.findOne({
+       _id: id,
+       _creator: req.user._id
+   }).then((todo) => {
        if (!todo) {
            return res.status(404).send();
        }
@@ -55,14 +58,17 @@ app.get('/todos/:id', (req, res) => {
    })
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send(`Id ${id} is invalid!`);
     }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if(!todo) {
             return res.status(404).send();
         }
@@ -72,7 +78,7 @@ app.delete('/todos/:id', (req, res) => {
     })
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
 
@@ -87,7 +93,10 @@ app.patch('/todos/:id', (req, res) => {
         body.completed = false;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.findOneAndUpdate({
+        _id: id,
+        _creator: req.user._id
+    }, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send()
         };
