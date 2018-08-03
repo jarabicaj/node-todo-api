190ebd4b46dc0e0974234f7ec1f107189e9a4faf
jarabicaj <jarabica.j@gmail.com>
2018-08-03 16:09:44 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,22 @@
 var env = process.env.NODE_ENV || 'development';
 //console.log('env *****', env);
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+if (env === 'development' || env === 'test'){
+    var config = require('./config.json');// auto parse in js object
+
+    //{ PORT: 3000, MONGODB_URI: 'mongodb://localhost:27017/TodoApp' }
+    var envConfig = config[env];
+
+    //Object.keys(envConfig); //[ 'PORT', 'MONGODB_URI' ]
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key]
+    })
 }
+
+// if (env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+// }
